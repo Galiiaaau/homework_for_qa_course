3f59c2746fdd452786c9218fbf10fb9d01d9cd5f
Galiia Uzbekova <galiiauzbekova@gmail.com>
2022-05-03 19:29:57 +0500

    added 4th task

--- fourTasks_JS.js
+++ fourTasks_JS.js
@@ -93,3 +93,172 @@ function findUniqueUsers() {
       return uniquePeople;
 }
 console.log(findUniqueUsers());
+
+// TASK 3**
+const enterprises = [{
+            id: 1,
+            name: "Предприятие 1",
+            departments: [{
+
+                        id: 2,
+                        name: "Отдел тестирования",
+                        employees_count: 10,
+                  },
+                  {
+                        id: 3,
+                        name: "Отдел маркетинга",
+                        employees_count: 20,
+                  },
+                  {
+                        id: 4,
+                        name: "Администрация",
+                        employees_count: 15,
+                  },
+            ]
+      },
+      {
+            id: 5,
+            name: "Предприятие 2",
+            departments: [{
+                        id: 6,
+                        name: "Отдел разработки",
+                        employees_count: 50,
+                  },
+                  {
+                        id: 7,
+                        name: "Отдел маркетинга",
+                        employees_count: 20,
+                  },
+                  {
+                        id: 8,
+                        name: "Отдел охраны труда",
+                        employees_count: 5,
+                  },
+            ]
+      },
+      {
+            id: 9,
+            name: "Предприятие 3",
+            departments: [{
+                  id: 10,
+                  name: "Отдел аналитики",
+                  employees_count: 0,
+            }, ]
+      }
+]
+
+// 1.
+function showAllEnterprisesInfo() {
+      let departments = '';
+      for (let i = 0; i < enterprises.length; i++) {
+            departments += `${enterprises[i].name} `;
+            if (enterprises[i].departments) {
+                  let count = 0;
+                  enterprises[i].departments.map(dep => count += dep.employees_count);
+                  count !== 0 ? departments += `(${count} сотрудников) \n` : departments += `(нет сотрудников) \n`;
+                  for (let k = 0; k < enterprises[i].departments.length; k++) {
+                        enterprises[i].departments[k].employees_count !== 0 ? departments += `- ${enterprises[i].departments[k].name} (${enterprises[i].departments[k].employees_count} сотрудников) \n` : departments += `- ${enterprises[i].departments[k].name} (нет сотрудников) \n`;
+                  }
+            }
+      }
+      return departments;
+}
+
+console.log(showAllEnterprisesInfo())
+
+// TASK 4
+const company = [{
+      id: 1,
+      name: "Компания",
+      parent: null,
+      users_count: 10,
+      children: [{
+                  id: 2,
+                  name: "Отдел тестирования",
+                  parent: 1,
+                  users_count: 7,
+                  children: [{
+                              id: 2,
+                              name: "Тестирование Web",
+                              parent: 2,
+                              users_count: 5,
+                        },
+                        {
+                              id: 3,
+                              name: "Тестирование Mobile",
+                              parent: 2,
+                              users_count: 0,
+                        },
+                  ]
+            },
+            {
+                  id: 4,
+                  name: "Отдел маркетинга",
+                  parent: 1,
+                  users_count: 30,
+            },
+            {
+                  id: 5,
+                  name: "Администрация",
+                  parent: 1,
+                  users_count: 25,
+                  children: [{
+                              id: 6,
+                              name: "Бухгалтерия",
+                              parent: 5,
+                              users_count: 10,
+                        },
+                        {
+                              id: 7,
+                              name: "Менеджмент",
+                              parent: 5,
+                              users_count: 15,
+                              children: [{
+                                          id: 8,
+                                          name: "Подраздел менеджмента 1",
+                                          parent: 7,
+                                          users_count: 5,
+                                    },
+                                    {
+                                          id: 9,
+                                          name: "Подраздел менеджмента 2",
+                                          parent: 7,
+                                          users_count: 10,
+                                    }
+                              ]
+                        },
+                        {
+                              id: 10,
+                              name: "HR",
+                              parent: 5,
+                              users_count: 1,
+                        }
+                  ]
+            },
+      ]
+}]
+
+function getDepartments() {
+      let departments = '';
+      for (let i = 0; i < company.length; i++) {
+            departments += `${company[i].name} ${company[i].users_count} \n`;
+            if (company[i].children) {
+                  for (let k = 0; k < company[i].children.length; k++) {
+                        departments += `-- ${company[i].children[k].name} (${company[i].children[k].users_count}) \n`;
+                        if (company[i].children[k].children) {
+                              for (let j = 0; j < company[i].children[k].children.length; j++) {
+                                    departments += `----${company[i].children[k].children[j].name} (${company[i].children[k].children[j].users_count}) \n`;
+                                    if (company[i].children[k].children[j].children) {
+                                          for (let g = 0; g < company[i].children[k].children[j].children.length; g++) {
+                                                departments += `------${company[i].children[k].children[j].children[g].name} (${company[i].children[k].children[j].children[g].users_count}) \n`;
+                                          }
+                                    }
+                              }
+                        }
+                  }
+            }
+      }
+      return departments;
+}
+
+console.log(getDepartments(company));
